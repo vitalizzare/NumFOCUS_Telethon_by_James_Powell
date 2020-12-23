16c9e9ec3af9f677c133f67bed08e7d570c05ca0
Vitalizzare <vitalizzare@gmail.com>
2020-12-23 04:34:27 +0200

    update C_contextmanagers

--- code_snippets/C_contextmanagers.py
+++ code_snippets/C_contextmanagers.py
@@ -1,19 +1,28 @@
-''' MOCK EXAMPLE:
+'''
+From the @NumFOCUS telethon
 
-from fake_db_api import connect
-
-with connect() as db:
-    with db.transaction() as tx:
-        db.execute('...')
-    with db.transaction() as tx:
-        db.execute('...')
-        db.execute('...')
-        with db.transaction() as tx:
-            db.execute('...')
+title: C for contextmanager
+author: @dontusethiscode
+date: December 19, 2020
 '''
 
+# MOCK EXAMPLE:
+# 
+#    from fake_db_api import connect
+#    
+#    with connect() as db:
+#        with db.transaction() as tx:
+#            db.execute('...')
+#        with db.transaction() as tx:
+#            db.execute('...')
+#            db.execute('...')
+#            with db.transaction() as tx:
+#                db.execute('...')
+
+# CASE 1: USE CONTEXTMANAGER FROM CONTEXTLIB
 from contextlib import contextmanager
 from time import perf_counter
+from time import sleep
             
 @contextmanager
 def timed(msg):
@@ -24,19 +33,43 @@ def timed(msg):
     finally:
         # __exit__
         stop = perf_counter()
-        print(f'Elapsed \N{greek capital letter delta}t: {stop - start:.2f}s')
+        print(f'{msg:<15} \N{greek capital letter delta}t: {stop - start:.2f}s')
 
-from time import sleep
-
-'''
-Expected output:
-    Elapsed Δt: 1.00s
-    Elapsed Δt: 3.00s
-'''
-with timed('sleep(1)'):
+print('CASE 1')
+with timed('Outside elapsed'):
     sleep(1)
-    with timed('section: sleep(1)'):
+    with timed('Inside elapsed'):
         sleep(1)
     sleep(1)
 
 
+# CASE 2: THE SAME AS A CLASS
+class timed:
+
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __enter__(self):
+        self.start = perf_counter()
+
+    def __exit__(self, *_):
+        self.stop = perf_counter()
+        print(f'{self.msg:<15} \N{greek capital letter delta}t: '
+              f'{self.stop - self.start:.2f}s')
+
+print('CASE 2')
+with timed('Outside elapsed'):
+    sleep(1)
+    with timed('Inside elapsed'):
+        sleep(1)
+    sleep(1)
+
+
+# EXPECTED OUTPUT:
+#   
+#   CASE 1
+#   Inside elapsed  Δt: 1.00s
+#   Outside elapsed Δt: 3.00s
+#   CASE 2
+#   Inside elapsed  Δt: 1.00s
+#   Outside elapsed Δt: 3.00s
